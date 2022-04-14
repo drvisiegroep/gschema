7871edf573bae220af31811e202be5aa92df8046
drvisiegroep <78969608+drvisiegroep@users.noreply.github.com>
2022-04-14 08:09:40 +0200

    updates misc

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     const listBtn = document.querySelector('.list-btn');
     const saveBtn = document.querySelector('.save-btn');
-    const schemaList = document.querySelector('.schema-list');
-
+    const schemaLijst = document.querySelector('.schema-lijst');
+    
     listBtn.addEventListener('click', () => {
-        console.log(listSavedEntries())
+        listSavedEntries();
     });
 
     saveBtn.addEventListener('click', () => {
-        saveEntries(saveName())
+    if (schemaValidation() == false) {
+        return false
+    }
+        saveItem(saveName())
+        listSavedEntries()
     });
 
+    schemaLijst.addEventListener('click',function(e){
+        if(e.target && e.target.className == 'remove-btn btn'){
+            const itemDataName= e.target.parentNode.dataset.name
+            removeItem(itemDataName);
+            listSavedEntries()
+         }
+     });
+
     function serializeEntries() {
-        let textareas = document.querySelectorAll('textarea');
-        let entries = [];
+        let textareas = document.querySelectorAll('.schema-input');
+        let entries = {};
         textareas.forEach(textarea => {
-            entries.push(textarea.value);
+            entries[textarea.dataset.kenmerk] = textarea.value
         });
-        return entries;
+        return JSON.stringify(entries)
     }
 
     function saveName() {
@@ -25,14 +37,52 @@ window.addEventListener('DOMContentLoaded', (event) => {
         return saveName;
     }
 
-    function listSavedEntries() {
-        const items = {...localStorage}
-        return items;
+    function removeItem(name) {
+        localStorage.removeItem(name)
     }
 
-    
-    function saveEntries(name) {
-        localStorage.setItem(name,serializeEntries());
+    function listSavedEntries() {
+        const items = {...localStorage}
+        const schemaLijst = document.querySelector('.schema-lijst');
+        list = '';
+        for (const [itemName, value] of Object.entries(items)) {
+            list += `<div class="schema-item" data-name="${itemName}"><h2>${itemName}</h2>`;
+            for (const [kenmerk, entry] of Object.entries(JSON.parse(value))) {
+                    list += `<p><strong>${kenmerk}</strong>: ${entry}</p>`;
+            }
+            list += `<div class="remove-btn btn">Verwijder item</div></div>`;
+        }
+        schemaLijst.innerHTML = list;
     }
+    
+    function saveItem(name) {
+       localStorage.setItem(name,serializeEntries())
+    }
+
+    function schemaValidation() {
+        const textareas = document.querySelectorAll('.schema-input');
+        const validInputs = Array.from(textareas).filter( input => input.value !== "");
+        textareas.forEach(textarea => {
+           if(textarea.value == '') {
+                textarea.classList.add('error')
+            } else {
+                textarea.classList.remove('error');
+            }
+        });
+        if (validInputs.length == textareas.length) {
+            return true            
+        } else {
+            return false 
+        }
+
+        
+    }
+
+    // TODO:
+    // 1. Functie om te checken of de velden niet leeg zijn
+    // 2. button om alles te wissen
+    // 3. popup maken voor saven
+    // 4. HTML / CSS aanpassen voor card slider
+
 
 });
