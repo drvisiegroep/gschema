877461510ca0ed1ff015448cfcd7d63bb9f8bd60
drvisiegroep <78969608+drvisiegroep@users.noreply.github.com>
2022-04-15 22:40:20 +0200

    slide test

--- script.js
+++ script.js
@@ -72,8 +72,6 @@ window.addEventListener("DOMContentLoaded", (event) => {
     localStorage.clear();
   }
 
-  function deleteConfirmation() {}
-
   function serializeEntries() {
     let textareas = document.querySelectorAll(".schema-input");
     let entries = {};
@@ -146,5 +144,54 @@ window.addEventListener("DOMContentLoaded", (event) => {
   // - Bericht weergeven als er geen opgeslagen items zijn.
   // - berichtenlijst een close button geven
   // - betere popup verzinnen voor de opgeslagen lijst?
-  // - 
+  // - betere feedback voor gebruiker wanneer er een actie is gebeurd(saved, deleted, veld x niet goed ingevuld. etc.)
+
+  //
+  // Slidertest
+  //
+
+
+  const slider = document.querySelector(".items");
+  const slides = document.querySelectorAll(".item");
+  const button = document.querySelectorAll(".button");
+
+  let current = 0;
+  let prev = 4;
+  let next = 1;
+
+  for (let i = 0; i < button.length; i++) {
+    button[i].addEventListener("click", () => i == 0 ? gotoPrev() : gotoNext());
+  }
+
+  const gotoPrev = () => current > 0 ? gotoNum(current - 1) : gotoNum(slides.length - 1);
+
+  const gotoNext = () => current < 4 ? gotoNum(current + 1) : gotoNum(0);
+
+  const gotoNum = number => {
+    current = number;
+    prev = current - 1;
+    next = current + 1;
+
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].classList.remove("active");
+      slides[i].classList.remove("prev");
+      slides[i].classList.remove("next");
+    }
+
+    if (next == 5) {
+      next = 0;
+    }
+
+    if (prev == -1) {
+      prev = 4;
+    }
+
+    slides[current].classList.add("active");
+    slides[prev].classList.add("prev");
+    slides[next].classList.add("next");
+  }
+
+
+
+  
 });
