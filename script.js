1a9a2afe791cfd7b39fe404a1d635adf00695e26
drvisiegroep <78969608+drvisiegroep@users.noreply.github.com>
2022-04-15 21:54:49 +0200

    misc

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 window.addEventListener("DOMContentLoaded", (event) => {
+  //
+  // Declerations
+  //
   const listBtn = document.querySelector(".list-btn");
   const saveBtn = document.querySelector(".save-btn");
   const clearAllBtn = document.querySelector(".clearall-btn");
@@ -6,13 +9,16 @@ window.addEventListener("DOMContentLoaded", (event) => {
   const savePopup = document.querySelector(".popup");
   const savePopupOverlay = document.querySelector(".popup-overlay");
   const schemaLijst = document.querySelector(".schema-lijst");
-  let timeOut
+  let timeOut;
 
 
+  //
+  // Eventlisteners
+  //
   listBtn.addEventListener("click", () => {
+    schemaLijst.classList.toggle('active');
     listSavedEntries();
   });
-
 
   launchSavePopupBtn.addEventListener("click", () => {
     if (schemaValidation() == false) {
@@ -22,29 +28,26 @@ window.addEventListener("DOMContentLoaded", (event) => {
     savePopupOverlay.classList.add("active");
   });
 
-
   savePopupOverlay.addEventListener("click", () => {
     savePopup.classList.remove("active");
     savePopupOverlay.classList.remove("active");
+    schemaLijst.classList.remove('active');
   });
-
 
   clearAllBtn.addEventListener("mouseup", () => {
     clearTimeout(timeOut);
-    return
-  });
-
-
-  clearAllBtn.addEventListener("mousedown", () => {
-    console.log('first')
-    timeOut = window.setTimeout(() => {
-        clearAll();
-        listSavedEntries();
-        //Timeout moet gelijk zijn aan de transition voor de button in de css
-    },2000);
     return;
   });
 
+  clearAllBtn.addEventListener("mousedown", () => {
+    console.log("first");
+    timeOut = window.setTimeout(() => {
+      clearAll();
+      listSavedEntries();
+      //Timeout moet gelijk zijn aan de transition voor de button in de css
+    }, 2000);
+    return;
+  });
 
   saveBtn.addEventListener("click", () => {
     if (schemaValidation() == false) {
@@ -53,7 +56,6 @@ window.addEventListener("DOMContentLoaded", (event) => {
     saveItem(saveName());
     listSavedEntries();
   });
-
 
   schemaLijst.addEventListener("click", function (e) {
     if (e.target && e.target.className == "remove-btn btn") {
@@ -64,10 +66,12 @@ window.addEventListener("DOMContentLoaded", (event) => {
   });
 
 
+  //
+  // Functions
+  //
   function clearAll() {
     localStorage.clear();
   }
-
 
   function deleteConfirmation() {}
 
@@ -77,52 +81,52 @@ window.addEventListener("DOMContentLoaded", (event) => {
     textareas.forEach((textarea) => {
       entries[textarea.dataset.kenmerk] = strip(textarea.value);
     });
+
     return JSON.stringify(entries);
   }
-
 
   function saveName() {
     const saveName = document.querySelector("#schema-naam").value;
     return saveName;
   }
 
-
   function removeItem(name) {
     localStorage.removeItem(name);
   }
-
 
   function strip(html) {
     let doc = new DOMParser().parseFromString(html, "text/html");
     return doc.body.textContent || "";
   }
 
-
   function listSavedEntries() {
     const items = { ...localStorage };
     const schemaLijst = document.querySelector(".schema-lijst");
     list = "";
+
     for (const [itemName, value] of Object.entries(items)) {
       list += `<div class="schema-item" data-name="${itemName}"><h2>${itemName}</h2>`;
+
       for (const [kenmerk, entry] of Object.entries(JSON.parse(value))) {
         list += `<p><strong>${kenmerk}</strong>: ${entry}</p>`;
       }
+
       list += `<div class="remove-btn btn">Verwijder item</div></div>`;
     }
+
     schemaLijst.innerHTML = list;
   }
-
 
   function saveItem(name) {
     localStorage.setItem(name, serializeEntries());
   }
-
 
   function schemaValidation() {
     const textareas = document.querySelectorAll(".schema-input");
     const validInputs = Array.from(textareas).filter(
       (input) => input.value !== ""
     );
+
     textareas.forEach((textarea) => {
       if (textarea.value == "") {
         textarea.classList.add("error");
@@ -130,6 +134,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
         textarea.classList.remove("error");
       }
     });
+
     if (validInputs.length == textareas.length) {
       return true;
     } else {
